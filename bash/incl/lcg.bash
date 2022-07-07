45cf47cabd425b5ce8108f42c68a1baf9196d578
Henrik Bengtsson <hb@aroma-project.org>
2022-07-08 01:12:53 +0200

    Bash: Add is_port_free()

--- bash/incl/lcg.bash
+++ bash/incl/lcg.bash
@@ -49,3 +49,20 @@ lcg_port() {
 get_uid() {
     id -u
 }
+
+
+#' Check if TCP Port is Free
+#'
+#' Examples:
+#' is_port_free 4001
+#' is_free=$?
+#'
+#' Requirements:
+#' * nc
+is_port_free() {
+    local port=${1:?}
+    if nc -z 127.0.0.1 "$port"; then
+        return 1
+    fi
+    return 0
+}
