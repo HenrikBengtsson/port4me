89e8804b6290de6230c788c5f6f7c0cfc3881364
Henrik Bengtsson <hb@aroma-project.org>
2022-07-07 12:52:55 +0200

    Update R and Bash examples to acknowledge PORT4ME_SKIP

--- r/ex-lcg.R
+++ r/ex-lcg.R
@@ -1,7 +1,12 @@
 source("R/lcg.R")
 lcg_set_seed(get_uid())
 
+skip <- as.integer(Sys.getenv("PORT4ME_SKIP", "0"))
+stopifnot(is.numeric(skip), !is.na(skip))
+
 for (kk in 1:5) {
-  message(lcg_port())
+  port <- lcg_port()
+  if (kk <= skip) next
+  message(port)
 }
 
