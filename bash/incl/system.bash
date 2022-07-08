fceeac2232fbbe114eb3013a41029e109f5c1cd3
Henrik Bengtsson <hb@aroma-project.org>
2022-07-08 13:07:31 +0200

    Move get_uid() and is_port_free() to separate file

---
+++ bash/incl/system.bash
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+get_uid() {
+    id -u
+}
+
+
+#' Check if TCP Port is Free
+#'
+#' Examples:
+#' is_port_free 4001
+#' is_free=$?
+#'
+#' Requirements:
+#' * nc
+is_port_free() {
+    local -i port=${1:?}
+    if nc -z 127.0.0.1 "$port"; then
+        return 1
+    fi
+    return 0
+}
