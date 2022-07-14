ddaca880b5520494016da16e19b31d935932d6d0
Henrik Bengtsson <hb@aroma-project.org>
2022-07-15 00:04:54 +0200

    bash: Add java_hashCode()

---
+++ bash/incl/java_hashCode.bash
@@ -0,0 +1,69 @@
+#! /usr/bin/env bash
+
+#' Algorithm
+#' h[n] = s[1]*31^(n-1) + s[2]*31^(n-2) + ... + s[n]
+#' =>
+#' h[0] = 0
+#' h[1] = s[1]
+#' h[2] = h[1]*31 + s[2]
+#' h[3] = h[2]*31 + s[3]
+#' ...
+#' h[n] = h[n-1]*31 + s[n]
+#'
+#' Examples:
+#' $ java_hashCode ""
+#' 0
+#' $ java_hashCode "A"
+#' 65
+#' $ java_hashCode "Arnold"
+#' 1969563338
+#' $ java_hashCode "port4me - get the same, personal, free TCP port over and over"
+#' 1731535982
+#' $ java_hashCode "alice,rstudio"
+#' -606348900
+#'
+#' Adopted from https://stackoverflow.com/a/48863502/1072091
+#' To be validated via https://www.online-java.com/
+java_hashCode() {
+    local str="$1"
+    local -i MAX_UINT=$(( 2**32 ))
+    local -i MAX_INT=2147483647   ## +2^31-1
+    local -i MIN_INT=-2147483648  ## -2^31
+    local -i kk byte
+    local -i hash=0
+    for ((kk = 0; kk < ${#str}; kk++)); do
+        ## ASCII character to ASCII value
+        LC_TYPE=C printf -v byte "%d" "'${str:$kk:1}"
+        hash=$(( 31 * hash + byte ))
+        
+        ## Corce to signed int32 [-2^31,+2^31-1]
+        hash=$(( (hash - MIN_INT) % MAX_UINT ))
+        (( hash < 0 )) && hash=$(( hash + MAX_UINT ))
+        hash=$(( hash + MIN_INT ))
+#        printf "%2d. byte=%3d, hash=%.0f\n" $kk $byte $hash
+    done
+    
+    printf "%d" $hash
+}
+
+java_hashCode_uint32() {
+    local str="$1"
+    local -i MAX_UINT=$(( 2**32 ))
+    local -i MAX_INT=2147483647   ## +2^31-1
+    local -i MIN_INT=-2147483648  ## -2^31
+    local -i kk byte
+    local -i hash=0
+
+    for ((kk = 0; kk < ${#str}; kk++)); do
+        ## ASCII character to ASCII value
+        LC_TYPE=C printf -v byte "%d" "'${str:$kk:1}"
+        hash=$(( 31 * hash + byte ))
+        if (( hash > MAX_INT )); then
+            hash=$(( (hash + MIN_INT) % -MIN_INT ))
+        elif (( hash < MIN_INT )); then
+            hash=$(( (hash - MIN_INT) % -MIN_INT ))
+        fi
+    done
+    
+    printf "%d" $hash
+}
