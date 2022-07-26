169bf39ade3fa10e3190c76ab6bf29bca3576891
Henrik Bengtsson <hb@aroma-project.org>
2022-07-26 08:24:03 +0200

    incorporate can_port_be_opened.bash into port4me.bash

--- bash/incl/port4me.bash
+++ bash/incl/port4me.bash
@@ -1,5 +1,43 @@
 #! /usr/bin/env bash
 
+#' Check if TCP port can be opened
+#'
+#' Examples:
+#' can_port_be_opened 4001
+#' openable=$?
+#'
+#' Requirements:
+#' * either 'nc' or 'ss'
+can_port_be_opened() {
+    local -i port=${1:?}
+    
+    (( port < 1 || port > 65535 )) && error "Port is out of range [1,65535]: ${port}"
+    
+    ## Is port occupied?
+    if command -v nc > /dev/null; then
+        if nc -z 127.0.0.1 "$port"; then
+            return 1
+        fi
+    elif command -v ss > /dev/null; then
+        if ss -H -l -n src :"$port" | grep -q -E ":$port\b"; then
+            return 1
+        fi
+    else
+        error "Neither command 'nc' nor 'ss' is available on this host ($HOSTNAME)"
+    fi
+    
+    ## FIXME: A port can be free, but it might be that the user
+    ## don't have the right to open it, e.g. port 1-1023.
+    ## WORKAROUND: If non-root, assume 1-1023 can't be opened
+    if [[ "$EUID" != 0 ]]; then
+        if (( port < 1024 )); then
+            return 1
+        fi
+    fi
+    
+    return 0
+}
+
 #' Analogue to java_hashCode() but returns a non-signed integer
 string_to_uint() {
     local str="$1"
