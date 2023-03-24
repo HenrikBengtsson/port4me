a8f6c624e462ded139a8abefbc15d001c06f4044
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-03-23 18:43:14 -0700

    FIX port4me --user=alice --tool=jupyter-notebook example which showed the wrong port [#26] /ht @Finnventor

--- r/tests/port4me.R
+++ r/tests/port4me.R
@@ -17,6 +17,20 @@ stopifnot(
   all(ports == truth)
 )
 
+message('- port4me(user = "alice", tool = "jupyter-notebook")')
+truth <- 29525L
+ports <- port4me(user = "alice", tool="jupyter-notebook")
+print(ports)
+stopifnot(
+  is.integer(ports),
+  all(is.finite(ports)),
+  all(ports > 0L),
+  all(ports <= 65535L),
+  all(ports >= 1024L),
+  length(ports) == length(truth),
+  all(ports == truth)
+)
+
 truth <- c(30845, 19654, 32310, 63992, 15273, 31420, 62779, 55372, 24143, 41300)
 message(sprintf("- port4me(list = %d)", length(truth)))
 ports <- port4me(list = length(truth))
