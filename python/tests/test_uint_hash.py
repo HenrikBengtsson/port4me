920b7039403e0e3899c23f65984c0c3c9fac6020
Finnventor <50817575+Finnventor@users.noreply.github.com>
2023-06-20 19:19:21 -0700

    Create test_uint_hash.py

---
+++ python/tests/test_uint_hash.py
@@ -0,0 +1,13 @@
+from port4me import uint_hash
+
+def test_empty():
+    assert uint_hash('') == 0
+
+def test_A():
+    assert uint_hash('A') == 65
+
+def test_alice():
+    assert uint_hash('alice,rstudio') == 3688618396
+
+def test_long():
+    assert uint_hash('port4me - get the same, personal, free TCP port over and over") == 1731535982
