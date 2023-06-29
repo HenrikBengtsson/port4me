ff78d8e09d20a23fb956ccf7428b3ca98978483f
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2023-06-29 22:16:31 +0200

    Merge parse_cli_args.R intro cli.R

--- r/R/cli.R
+++ r/R/cli.R
@@ -21,3 +21,26 @@ parse_cli_args <- function() {
   
   args
 }
+
+
+#' @export
+print.cli_function <- function(x, ..., envir = parent.frame()) {
+  if (interactive()) return(NextMethod())
+
+  args <- parse_cli_args()
+  str(args)
+  
+  res <- withVisible(do.call(port4me, args = args, envir = envir))
+  
+  # Should the result be printed?
+  if (res$visible) {
+    value <- res$value
+    if (is.integer(value)) {
+      cat(sprintf("%i\n", value), collapse = "", sep = "")
+    } else if (is.logical(value)) {
+      cat(sprintf("%s\n", value), collapse = "", sep = "")
+    }
+  }
+
+  invisible()
+}
